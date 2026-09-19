ba5e7f54d1f3f82c2d08df2316e0eac66e488c53
Khant Wai Yan <52145576+WaiYan70@users.noreply.github.com>
2026-09-20 04:04:30 +0700

    add: PostEditorForm for post-editor-page

--- src/app/(admin)/admin/posts/[postId]/edit/page.tsx
+++ src/app/(admin)/admin/posts/[postId]/edit/page.tsx
@@ -1,3 +1,43 @@
-export default function EditPostPage() {
-  return <div>Edit Post Page</div>;
+import { updatePostAction } from "@/features/admin/posts/actions";
+import { PostEditorForm } from "@/features/admin/posts/components/PostEditorForm";
+import { getPostBySlug } from "@/features/blog/lib/post";
+import { requireAdmin } from "@/lib/auth/require-admin";
+import { notFound } from "next/navigation";
+
+type EditPageProps = {
+  params: Promise<{ postId: string }>;
+};
+
+export default async function EditPostPage({ params }: EditPageProps) {
+  await requireAdmin();
+
+  const { postId } = await params;
+  const post = await getPostBySlug(postId);
+  if (!post) {
+    notFound();
+  }
+
+  const action = updatePostAction.bind(null, post.slug);
+
+  return (
+    <main>
+      <header>
+        <h1>Edit Post Page - {post.slug}</h1>
+        <p>Update the meta and articel content.</p>
+      </header>
+      <PostEditorForm
+        key={post.slug}
+        mode="edit"
+        action={action}
+        defaultValues={{
+          title: post.title,
+          slug: post.slug,
+          description: post.description,
+          date: post.date,
+          tags: post.tags,
+          content: post.content,
+        }}
+      />
+    </main>
+  );
 }
